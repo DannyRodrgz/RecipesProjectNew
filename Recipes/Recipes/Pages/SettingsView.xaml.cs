52c7f8a45c59793e76d18aa62ddf4ec111428990
Danny Rodriguez - Jalasoft <danny.rodriguez.montecinos@gmail.com>
2019-01-23 00:05:40 -0400

    feature/settings-screen

---
+++ Recipes/Recipes/Pages/SettingsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.Forms.Views;
+using Recipes.ViewModels;
+
+namespace Recipes.Pages
+{
+    public partial class SettingsView : MvxContentPage<SettingsViewModel>
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
